124151223b7f9e5cd9043f8c2bc90e99239f343e
Kristin Barr <kristinbarr@gmail.com>
2019-10-14 17:35:59 -0400

    changed port for testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5002
 
 server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} *\n`)

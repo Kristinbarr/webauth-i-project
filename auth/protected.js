dc3cf9bc79d09b6a71d774de5cbd34cfe5e6603a
Kristin Barr <kristinbarr@gmail.com>
2019-10-15 19:43:41 -0400

    updated middleware to check session

--- auth/protected.js
+++ auth/protected.js
@@ -2,22 +2,25 @@ const Users = require('../users/user-model')
 const bcrypt = require('bcryptjs')
 
 module.exports = function protected(req, res, next) {
-  let { username, password } = req.headers
+  // let { usernamex, password } = req.headers
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then((user) => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next()
-        } else {
-          res.status(401).json({ message: 'You cannot pass!!' })
-        }
-      })
-      .catch((error) => {
-        res.status(500).json(error)
-      })
-  } else {
-    res.status(400).json({ message: 'please provide credentials' })
+  if (req.session && req.session.user) {
+    next()
+  // if (username && password) {
+    // Users.findBy({ username })
+    //   .first()
+    //   .then((user) => {
+    //     if (user && bcrypt.compareSync(password, user.password)) {
+    //       next()
+    //     } else {
+    //       res.status(401).json({ message: 'You cannot pass!!' })
+    //     }
+    //   })
+    //   .catch((error) => {
+      //     res.status(500).json(error)
+      //   })
+    } else {
+      res.status(401).json({ message: 'You cannot pass!!' })
+    // res.status(400).json({ message: 'please provide credentials' })
   }
 }
